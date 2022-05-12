7761a6a80820330a356cba253ff575316a7f4444
Eddie Duncan <carl.duncan@americancarcenter.com>
2022-05-12 18:08:16 -0500

    Completed Section 1 of Apple's SwiftUI Essentials.

---
+++ Landmarks/Views/Landmarks/LandmarkList.swift
@@ -0,0 +1,50 @@
+//
+//  landmarkList.swift
+//  Landmarks
+//
+//  Created by Carl Duncan on 5/11/22.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    @EnvironmentObject var modelData: ModelData
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter {
+            landmark in (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
+    
+    var body: some View {
+        
+        NavigationView {
+            List{
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites Only")
+                }
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone 13 Pro Max"], id: \.self){ deviceName in
+            LandmarkList()
+                .environmentObject(ModelData())
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+        }
+        
+    }
+}

a7c8a071ced7111b421c308ab661461cb80042f3
Eddie Duncan <carl.duncan@americancarcenter.com>
2022-05-11 21:12:43 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Carl Duncan on 5/11/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
